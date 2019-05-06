16bf6e8a2b686a40bfc428cd3b03d6bfde0eef32
intelfike <intelfike@gmail.com>
2019-05-06 14:28:01 +0900

    main.goのssl対応を緩和 ローカルファイルに対して有効化する手順を追記

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
@@ -9,12 +10,21 @@ import (
 	"golang.org/x/crypto/acme/autocert"
 )
 
-func main() {
-	pack := mulpage.NewPackage(&contents.App{})
+var https = flag.Bool("https", false, "Using HTTPS protocol. (default HTTP)")
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		mulpage.Handler(w, r, pack, "web")
-	})
-	log.Fatal(http.Serve(autocert.NewListener("isear.intelfike.net"), mux))
+var port = flag.String("port", ":80", "HTTP port number")
+
+func main() {
+	flag.Parse()
+	if *https {
+		pack := mulpage.NewPackage(&contents.App{})
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			mulpage.Handler(w, r, pack, "web")
+		})
+		log.Fatal(http.Serve(autocert.NewListener("isear.intelfike.net"), mux))
+		return
+	}
+	mulpage.All(&contents.App{}, "web", *port)
 }
